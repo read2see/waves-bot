3645f4695ab4ba64c5bcf389bcdc4131e22489ae
Hussain Al-Nokhitha <ips.hussain.79@outlook.com>
2023-04-08 15:50:29 +0300

    rearrange commands' options in a friendly manner.

--- commands/utils/screenshots.js
+++ commands/utils/screenshots.js
@@ -9,10 +9,17 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('screenshots')
 		.setDescription('Posts generated screenshots if they exist.')
+		.addChannelOption(option =>
+			option.setName('channel')
+				.setDescription('The channel to reply at. defaults to current channel.')
+		)
 		.addStringOption( option => 
 			option.setName('on-completion')
 			.setDescription('Set custom completion message.')
-			
+		)
+		.addBooleanOption(option =>
+			option.setName('visible-only-to-me')
+			.setDescription('Set the bots message visibility to yourself alone.')
 		)
 		.addStringOption( option => 
 			option.setName('exception-1')
@@ -47,13 +54,6 @@ module.exports = {
 				{name: 'omv-l', value: 'omv-l'},
 			)
 		)
-		.addBooleanOption(option =>
-			option.setName('visible-only-to-me')
-			.setDescription('Set the bots message visibility to yourself alone.')
-		)
-		.addChannelOption(option =>
-			option.setName('channel')
-				.setDescription('The channel to reply at. defaults to current channel.'))
 	,
 	async execute(interaction) {
 		await interaction.deferReply({ephemeral: interaction.options.getBoolean('visible-only-to-me')});
--- commands/utils/update.js
+++ commands/utils/update.js
@@ -76,6 +76,18 @@ module.exports = {
 				option.setName('version')
 					.setDescription('Game version.')
 				)
+			.addChannelOption(option =>
+				option.setName('channel')
+					.setDescription('The channel to reply at. defaults to current channel.')
+					)
+			.addStringOption( option => 
+				option.setName('on-completion')
+				.setDescription('Set custom completion message.')
+			)
+			.addBooleanOption(option =>
+				option.setName('visible-only-to-me')
+				.setDescription('Set the bot\'s message visibility to yourself alone.')
+			)
 			.addStringOption( option => 
 			option.setName('exception-1')
 			.setDescription('Choose a screenshot to exclude.')
@@ -109,23 +121,10 @@ module.exports = {
 				{name: 'omv-l', value: 'omv-l'},
 			)
 		)
-		.addStringOption( option => 
-			option.setName('on-completion')
-			.setDescription('Set custom completion message.')
-			
-		)
-		.addBooleanOption(option =>
-			option.setName('visible-only-to-me')
-			.setDescription('Set the bots message visibility to yourself alone.')
-		)
 		.addBooleanOption(option =>
             option.setName('delete-last-post')
             .setDescription('Deletes latest bot post if the user is attempting to correct generated screenshots.')
         )
-		.addChannelOption(option =>
-			option.setName('channel')
-				.setDescription('The channel to reply at. defaults to current channel.')
-				)
 		,
 
 	async execute(interaction) {
